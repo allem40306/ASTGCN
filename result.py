8de166a14a2029ca7b655a6c232ebc7225367117
allem40306 <allem40306@gmail.com>
2023-06-06 19:27:57 +0800

    新增功能：輸出 prediction 結果

---
+++ result.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def mask_np(array, null_val):
+    if np.isnan(null_val):
+        return (~np.isnan(null_val)).astype('float32')
+    else:
+        return np.not_equal(array, null_val).astype('float32')
+
+
+def masked_mape_np(y_true, y_pred, null_val=np.nan):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        mask = mask_np(y_true, null_val)
+        mask /= mask.mean()
+        mape = np.abs((y_pred - y_true) / y_true)
+        mape = np.nan_to_num(mask * mape)
+        return np.mean(mape) * 100
+
+
+def masked_mse_np(y_true, y_pred, null_val=np.nan):
+    mask = mask_np(y_true, null_val)
+    mask /= mask.mean()
+    mse = (y_true - y_pred) ** 2
+    return np.mean(np.nan_to_num(mask * mse))
+
+
+def masked_mae_np(y_true, y_pred, null_val=np.nan):
+    mask = mask_np(y_true, null_val)
+    mask /= mask.mean()
+    mae = np.abs(y_true - y_pred)
+    return np.mean(np.nan_to_num(mask * mae))
+
+with np.load("ASTGCN_prediction_08.npz") as data:
+    x = data["prediction"]
+    y = data["ground_truth"]
+    mae, mape, rmse = masked_mae_np(y, x, 0), masked_mape_np(y, x, 0), masked_mse_np(y, x, 0) ** 0.5
+    print('test:, MAE: {:.2f}, MAPE: {:.2f}, RMSE: {:.2f}'.format(mae, mape, rmse),flush=True)
